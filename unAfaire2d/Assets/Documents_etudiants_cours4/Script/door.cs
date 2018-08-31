aa131a422e7e03d18fbe8b4361040b2b32e06a9a
B533P-16\533 <acv@hotmail.com>
2018-08-31 16:19:26 -0400

    projet 2d

---
+++ unAfaire2d/Assets/Documents_etudiants_cours4/Script/door.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class door : MonoBehaviour {
+    [SerializeField] string sceneName;
+    [SerializeField] AudioClip stairsSound;
+
+
+    AudioSource AudioSource;
+	// Use this for initialization
+	void Start () {
+        AudioSource.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AudioSource.PlayOneShot(stairsSound);
+        Invoke("changeLevel",1.0f);
+    }
+
+
+    private void changeLevel()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
